4d7037c0f09941a1531fa1e51e41aa7dbf7716b7
sarojdebata <sarojdebata21@gmail.com>
2018-12-19 00:25:42 +0530

    file upload download project

---
+++ file-upload-download/src/main/java/com/spring/fileHandling/fileuploaddownload/FileUploadDownloadApplication.java
@@ -0,0 +1,16 @@
+package com.spring.fileHandling.fileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages="com.spring")
+public class FileUploadDownloadApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(FileUploadDownloadApplication.class, args);
+	}
+
+}
+
